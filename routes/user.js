63ee7e1b00fbb43c878ef9082dea7f70dce1c4ad
Michael <MichaelSmith90Dev@gmail.com>
2025-01-11 11:54:16 +0000

    Add controller callbacks to routes

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,26 @@
 const Router = require("express").Router;
+const user = require("../controllers/user");
 
 const router = new Router();
 
 // POST /Signup
+router.get("/signup", user.signup);
 
 // POST /Login
+router.get("/login", user.login);
 
 // POST /RefreshToken
+router.get("/refresh", user.refreshToken);
 
-// GET /:userid single - restricted to user
+// GET /:userId
+router.get(
+  "/:userId",
+  (req, res, next) => next(), // Add user check,
+  user.getUserById,
+);
 
 // GET all - restricted to admin
+router.get("/", user.SearchUsers);
 
 // PUT single - restricted to user
 
