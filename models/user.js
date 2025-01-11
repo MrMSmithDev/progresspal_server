77040cee5b6b0a1d5022640e0acd09e54b7a6aba
Michael <MichaelSmith90Dev@gmail.com>
2025-01-11 16:56:35 +0000

    Add length requirements for username/email/target

--- models/user.js
+++ models/user.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true, min: 3, max: 30 },
+  email: { type: String, required: true, unique: true, min: 5 },
   hash: { type: String, required: true },
   salt: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
-  target: { type: Number, max: 28, default: null },
+  target: { type: Number, min: 1, max: 28, default: null },
   admin: { type: Boolean, default: false },
   workouts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Workout" }],
 });
