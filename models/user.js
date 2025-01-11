d7655192cd2eefa6b286ccb54ecabbfd5cfcb599
Michael <MichaelSmith90Dev@gmail.com>
2025-01-11 11:54:41 +0000

    Add model fields and type requirements

--- models/user.js
+++ models/user.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  // email
-  // hash
-  // salt
-  // Workouts
+  email: { type: String, required: true, unique: true },
+  hash: { type: String, required: true },
+  salt: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  target: { type: Number, max: 28, default: null },
+  admin: { type: Boolean, default: false },
+  workouts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Workout" }],
 });
 
 module.exports = mongoose.model("user", UserSchema);
--- models/workout.js
+++ models/workout.js
@@ -1,9 +1,39 @@
 const mongoose = require("mongoose");
 
 const WorkoutSchema = new mongoose.Schema({
-  user: [{ type: mongoose.Types.ObjectId, ref: "user" }],
-  // date
-  // length (hours:mins)
+  userId: [{ type: mongoose.Types.ObjectId, ref: "User", required: true }],
+  date: { type: Date, required: true },
+  length: { type: Number, required: true, min: 1 },
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: true,
+        enum: ["strength", "cardio"],
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      sets: [
+        {
+          weight: { type: Number, min: 0 },
+          reps: { type: Number, min: 1 },
+        },
+      ],
+      distance: {
+        type: Number,
+        min: 0,
+        validate: {
+          validator: function (value) {
+            // Make required only for cardio type activities
+            return this.type === "cardio" ? value !== undefined : true;
+          },
+          message: "Distance is required for cardio exercises",
+        },
+      },
+    },
+  ],
 });
 
 module.exports = mongoose.model("workout", WorkoutSchema);
