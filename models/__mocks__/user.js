78b307c5e892de65626cfd0859b15ce5c0c7f723
Michael <MichaelSmith90Dev@gmail.com>
2025-01-12 16:10:18 +0000

    Adjust mock to allow for non class based use

--- models/__mocks__/user.js
+++ models/__mocks__/user.js
@@ -1,8 +1,22 @@
 /* eslint-disable no-undef */
-module.exports = jest.fn(() => ({
-  save: jest.fn(() => ({
+const User = jest.fn((userData) => ({
+  ...userData,
+  save: jest.fn().mockResolvedValue({
     _id: "test_id",
     username: "test_username",
     email: "test@email.com",
-  })),
+  }),
 }));
+
+User.findById = jest.fn(() => ({
+  _id: "test_id",
+  username: "test_username",
+  email: "test@email.com",
+  toObject: jest.fn().mockReturnValue({
+    _id: "test_id",
+    username: "test_username",
+    email: "test@email.com",
+  }),
+}));
+
+module.exports = User;
