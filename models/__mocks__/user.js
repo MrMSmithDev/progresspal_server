a1980ceb90122cb8a98f78038c5eeabfb1d444f3
Michael <MichaelSmith90Dev@gmail.com>
2025-01-12 17:40:58 +0000

    Add updateOne mock func

--- models/__mocks__/user.js
+++ models/__mocks__/user.js
@@ -19,4 +19,15 @@ User.findById = jest.fn(() => ({
   }),
 }));
 
+User.limit = jest.fn(() => [
+  { _id: "test_id", username: "test_username", email: "test@email.com" },
+  { _id: "test_id2", username: "test_username2", email: "test@email.com2" },
+]);
+
+User.find = jest.fn(() => ({
+  limit: User.limit,
+}));
+
+User.updateOne = jest.fn(() => "docid-5");
+
 module.exports = User;
