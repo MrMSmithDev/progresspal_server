d2095511fe87072a1a4a427e781dfbfecc3746c7
Michael <MichaelSmith90Dev@gmail.com>
2025-01-14 12:00:25 +0000

    Add route structuring for workouts

--- routes/workout.js
+++ routes/workout.js
@@ -1,15 +1,22 @@
+const { isCreator, isAuth } = require("../middleware");
+
 const Router = require("express").Router;
 
 const router = new Router();
 
-// GET /:workoutId - single workout
+// GET /:workoutId single workout
+router.get("/:workoutId", () => {});
 
-// GET / all workouts - restricted to user
+// GET / all workouts for user - restricted to user
+router.get("/", isAuth, () => {});
 
-// POST / - restricted to currentUser
+// POST / - restricted to user
+router.post("/", isAuth, () => {});
 
-// PUT /:workoutId - restricted to user
+// PUT /:workoutId - restricted to creator
+router.put("/:workoutId", isCreator, () => {});
 
-// DELETE / - restricted to user
+// DELETE / - restricted to creator
+router.delete("/workoutId", isCreator, () => {});
 
 module.exports = router;
