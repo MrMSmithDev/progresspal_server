ccb751f2fb672e945561b5975b63bd59cf97b50e
Michael <MichaelSmith90Dev@gmail.com>
2025-01-14 17:11:26 +0000

    Assign controller funcs to routes

--- routes/workout.js
+++ routes/workout.js
@@ -1,22 +1,23 @@
 const { isCreator, isAuth } = require("../middleware");
+const workout = require("../controllers/workout");
 
 const Router = require("express").Router;
 
 const router = new Router();
 
 // GET /:workoutId single workout
-router.get("/:workoutId", () => {});
+router.get("/:workoutId", workout.getWorkoutById);
 
 // GET / all workouts for user - restricted to user
-router.get("/", isAuth, () => {});
+router.get("/", isAuth, workout.getUsersWorkouts);
 
 // POST / - restricted to user
-router.post("/", isAuth, () => {});
+router.post("/", isAuth, workout.createWorkout);
 
 // PUT /:workoutId - restricted to creator
-router.put("/:workoutId", isCreator, () => {});
+router.put("/:workoutId", isCreator, workout.editWorkout);
 
 // DELETE / - restricted to creator
-router.delete("/workoutId", isCreator, () => {});
+router.delete("/workoutId", isCreator, workout.deleteWorkout);
 
 module.exports = router;
