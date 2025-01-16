ca609a78b06a3de38c98f7847cf13bfe2aa2f6cb
Michael <MichaelSmith90Dev@gmail.com>
2025-01-16 16:29:54 +0000

    Implement caching

--- controllers/workout.js
+++ controllers/workout.js
@@ -1,8 +1,10 @@
 const Workout = require("../models/workout");
 const mongoose = require("mongoose");
+const cache = require("../config/cache");
 
 const { verifyWorkoutInput } = require("../lib/verification");
 const { convertExerciseToInt } = require("../lib/convertDataTypes");
+const { createCacheKey } = require("../lib/cacheUtils");
 
 // GET /:workoutId - single workout
 module.exports.getWorkoutById = async function (req, res) {
@@ -31,28 +33,53 @@ module.exports.getWorkoutById = async function (req, res) {
 // GET / all workouts - restricted to user
 module.exports.getUsersWorkouts = async function (req, res) {
   const { userId } = req.params;
-  const { limit = "20" } = req.query;
+  const { limit = "20", skip = "0" } = req.query;
 
-  // Check limit is valid and set to default if not
+  // Check limit and skip are valid and set to default if not
   let parsedLimit = parseInt(limit, 10);
+  let parsedSkip = parseInt(skip, 10);
   if (!/^\d+$/.test(req.query.limit) || isNaN(parsedLimit)) parsedLimit = 20;
+  if (!/^\d+$/.test(req.query.skip) || isNaN(parsedSkip)) parsedSkip = 0;
 
   if (!mongoose.Types.ObjectId.isValid(userId))
     return res.status(400).json({ error: `Invalid user ID: ${userId}` });
 
-  try {
-    const result = await Workout.find({ userId })
-      .sort({ createdAt: "desc" })
-      .limit(parsedLimit);
+  const cacheKey = createCacheKey("getUsersWorkouts", {
+    userId,
+    limit: parsedLimit,
+    skip: parsedSkip,
+  });
 
-    if (result.length <= 0) return res.json([]);
-    return res.json(result);
-  } catch (err) {
-    console.log(err);
-    return res
-      .status(500)
-      .json({ error: `Internal server error: ${err.message}` });
-  }
+  cache.get(cacheKey, async (err, cachedData) => {
+    if (err) {
+      console.log(err);
+      return res
+        .status(500)
+        .json({ error: `Internal server error: ${err.message}` });
+    }
+
+    if (cachedData) {
+      return res.json(JSON.parse(cachedData));
+    }
+
+    try {
+      const result = await Workout.find({ userId })
+        .sort({ createdAt: "desc" })
+        .skip(parsedSkip)
+        .limit(parsedLimit);
+
+      if (result.length <= 0) return res.json([]);
+
+      cache.setex(cacheKey, 1800, JSON.stringify(result));
+
+      return res.json(result);
+    } catch (err) {
+      console.log(err);
+      return res
+        .status(500)
+        .json({ error: `Internal server error: ${err.message}` });
+    }
+  });
 };
 
 // POST / - restricted to currentUser
