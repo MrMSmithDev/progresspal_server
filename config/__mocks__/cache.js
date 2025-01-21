c08ff3e56833f0ff0a812f32f95daedc2b703924
Michael <MichaelSmith90Dev@gmail.com>
2025-01-21 17:19:28 +0000

    Add redis mocking to cache mock

--- config/__mocks__/cache.js
+++ config/__mocks__/cache.js
@@ -1,8 +1,11 @@
 /* eslint-disable no-undef */
+jest.mock("redis");
+
 const mockClient = {
   get: jest.fn((key, callback) => callback(null, null)),
   setex: jest.fn(),
   on: jest.fn(),
+  del: jest.fn(),
 };
 
 module.exports = mockClient;
