301302cb5eaf62194a79c4852bc018dc1ff8f37d
Michael <MichaelSmith90Dev@gmail.com>
2025-01-30 11:50:08 +0000

    Add weight creator locating

--- __tests__/lib/findCreatorId.test.js
+++ __tests__/lib/findCreatorId.test.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-undef */
 const findCreatorId = require("../../lib/findCreatorId");
 const Workout = require("../../models/workout");
+const Weight = require("../../models/weight");
 
 jest.mock("../../models/workout");
+jest.mock("../../models/weight");
 
 describe("findCreatorId", () => {
   afterEach(() => {
@@ -12,7 +14,7 @@ describe("findCreatorId", () => {
   it("should return the creator ID when a valid workoutId is provided", async () => {
     const mockCreatorId = { userId: "12345" };
 
-    const result = await findCreatorId("validWorkoutId");
+    const result = await findCreatorId("validWorkoutId", "workout");
     expect(Workout.findOne).toHaveBeenCalledWith({ _id: "validWorkoutId" });
     expect(Workout.findOne).toHaveBeenCalledWith(
       expect.objectContaining({ _id: "validWorkoutId" }),
@@ -21,10 +23,22 @@ describe("findCreatorId", () => {
     expect(result).toEqual(mockCreatorId.userId);
   });
 
+  it("should return the creator ID when a valid weightId is provided", async () => {
+    const mockCreatorId = { userId: "12345" };
+
+    const result = await findCreatorId("validWeightId", "weight");
+    expect(Weight.findOne).toHaveBeenCalledWith({ _id: "validWeightId" });
+    expect(Weight.findOne).toHaveBeenCalledWith(
+      expect.objectContaining({ _id: "validWeightId" }),
+    );
+    expect(Weight.findOne().select).toHaveBeenCalled();
+    expect(result).toEqual(mockCreatorId.userId);
+  });
+
   it("should return undefined if the workout is not found", async () => {
     Workout.findOne().select.mockResolvedValueOnce(null);
 
-    const result = await findCreatorId("nonExistentWorkoutId");
+    const result = await findCreatorId("nonExistentWorkoutId", "workout");
     expect(Workout.findOne).toHaveBeenCalledWith({
       _id: "nonExistentWorkoutId",
     });
@@ -37,7 +51,7 @@ describe("findCreatorId", () => {
 
     const consoleSpy = jest.spyOn(console, "log").mockImplementation(() => {});
 
-    const result = await findCreatorId("errorWorkoutId");
+    const result = await findCreatorId("errorWorkoutId", "workout");
     expect(Workout.findOne).toHaveBeenCalledWith({ _id: "errorWorkoutId" });
     expect(consoleSpy).toHaveBeenCalledWith(
       `Error finding creator ID: ${mockError.message}`,
