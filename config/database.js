3a2ddd62b1f965bee06a73a96aa8fe87d5c92cb6
Michael <MichaelSmith90Dev@gmail.com>
2025-02-12 11:32:51 +0000

    Adjust env variables names

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
-const mongoDB = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@development-progress-pa.9rasp.mongodb.net/?retryWrites=true&w=majority&appName=development-Progress-Pal`;
+const mongoDB = `mongodb+srv://${process.env.DATABASE_UN}:${process.env.DATABASE_PW}@development-progress-pa.9rasp.mongodb.net/?retryWrites=true&w=majority&appName=development-Progress-Pal`;
 
 async function connectDB() {
   await mongoose.connect(mongoDB);
